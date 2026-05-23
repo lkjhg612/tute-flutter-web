f67012b4bdc75b756a57f82b7ea08b6ecae76e54
khang <khang@192.168.1.17>
2026-05-23 20:06:00 +0700

    Force browser title

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Cơm Tấm Chay TUTE',
       debugShowCheckedModeBanner: false,
       //home: Homepage(),
       initialRoute: '/',
